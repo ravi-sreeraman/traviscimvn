8ead7830e6dbfdfe49aa6cb8ad2105a9ca174eb6
pinturavi <pintu541796@gmail.com>
2019-09-17 22:35:00 +0530

    initial change

---
+++ src/test/java/com/pintu/AppTest.java
@@ -0,0 +1,28 @@
+package com.pintu;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldReturnSum(){
+        assertEquals(5, App.add(3, 2));
+        assertEquals(10, App.add(5, 5));
+    }
+}
